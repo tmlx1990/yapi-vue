1fbb7206499c63c3c444e7d519707e5149086f26
tmlx1990 <tmlx1990@163.com>
2023-11-20 17:12:11 +0800

    重新初始化项目

--- yapi-vue/src/main.js
+++ yapi-vue/src/main.js
@@ -1,6 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import { Button, message} from "ant-design-vue";
+const app = createApp(App);
+app.use(Button).mount('#app');
 
-createApp(App).mount('#app')
+app.config.globalProperties.$message = message;
